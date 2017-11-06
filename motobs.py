6fba24753dc6cc018ecbdc26aa8cf63420c66ad4
olekvin <33288640+olekvin@users.noreply.github.com>
2017-11-06 11:07:14 +0100

    Update motobs.py

--- motobs.py
+++ motobs.py
@@ -13,10 +13,21 @@ class Motob:
     def operationalize(self):
         for rec in self.value:
             if rec[0] == "F":
-                motors.forward(speed=rec[1]*0.01)
+                self.motors.forward(speed=rec[1]*0.01)
             elif rec[0] == "B":
-                motors.backward(speed=rec[1]*0.01)
+                self.motors.backward(speed=rec[1]*0.01)
+            elif rec[0] == "L":
+                pass
+            elif rec[0] == "R":
+                pass
+            elif rec[0] == "TL":
+                self.motors.set_value([[rec[1]],[0]])
+            elif rec[0] == "TR":
+                self.motors.set_value([[0],[rec[1]]])
+
+
 
 
     def turnDegrees(deg):
         return 0.0028 * deg #Så mye snur den visst
+
