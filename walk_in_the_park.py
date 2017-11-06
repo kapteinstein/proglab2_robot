36a40a71fb536f85b545306c30cab7210ab38e2c
Anders <andersogranaas@gmail.com>
2017-11-06 13:42:07 +0100

    changes

--- walk_in_the_park.py
+++ walk_in_the_park.py
@@ -22,8 +22,7 @@ class WalkInThePark(Behavior):
         pass
 
     def sense_and_act(self):
-        moves = [('L', 50), ("R", 50), ("TL", 50), ('TR', 50), ("F", 20), ("B", 20),
-        ('L', -50), ("R", -50), ("TL", -50), ('TR', -50)]
+        moves = [('L', 50), ("R", 50), ("TL", 50), ('TR', 50), ("F", 20), ("B", 20)]
         for i in range(len(moves)):
             self.motor_recommendations = moves[i]
 
