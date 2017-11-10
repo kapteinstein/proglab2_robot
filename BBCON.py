e87d9b4a9fa2e33da609df11e98eff419efbe25b
Erik Liodden <eriklio@stud.ntnu.no>
2017-11-10 11:58:33 +0100

    fix typo

--- BBCON.py
+++ BBCON.py
@@ -75,7 +75,7 @@ class BBCON():
         if len(sys.argv) > 1 and sys.argv[1] == '-v':
             print("behaviors: ")
             for behavior in self.active_behaviors:
-                print("{0: <16}: ".format(bahavior.name), end='')
+                print("{0: <16}: ".format(behavior.name), end='')
                 print("weight: {:.4f},\tmotor_rec:{}".format(behavior.weight,
                     behavior.motor_recommendations))
             print("--> action: {}".format(result))
